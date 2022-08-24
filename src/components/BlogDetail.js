fc0d4204987825d1a188afd47b67fbb2ee99ad1e
maaammos <moss10612@gmail.com>
2022-08-24 11:39:39 +0700

    change path

--- src/components/BlogDetail.js
+++ src/components/BlogDetail.js
@@ -16,7 +16,7 @@ const BlogDetail = () => {
     };
     const fetchDetails = async () => {
         const res = await axios
-            .get(`https://fitplus-project-frontend.vercel.app/api/blog/${id}`)
+            .get(`https://fitplus-project-backend.vercel.app/api/blog/${id}`)
             .catch((err) => console.log(err));
         const data = await res.data;
         return data;
